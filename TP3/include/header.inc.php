0ed925c2a5fce6609872bcdf57f3513cf011757c
harold <leroyharold@sfr.fr>
2020-04-08 23:11:55 +0200

    menu

--- TP3/include/header.inc.php
+++ TP3/include/header.inc.php
@@ -39,3 +39,6 @@
 
   </header>
 <body>
+
+
+  
---
+++ TP3/include/menu.inc.php
@@ -0,0 +1,13 @@
+<ul class="menu">
+            <li><a href="#">Menu 1</a></li>
+            <li><a href="#">Menu 2</a>
+                <ul>
+                    <li><a href="#">Sous-Menu 2.1</a></li>
+                    <li><a href="#">Sous-Menu 2.2</a></li>
+                    <li><a href="#">Sous-Menu 2.3</a></li>
+                    <li><a href="#">Sous-Menu 2.4</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Menu 3</a></li>
+            <li><a href="#">Menu 4</a></li>
+        </ul>
