b32db98d73a67fb344f5ce1e990a03f50e29728c
harold <leroyharold@sfr.fr>
2020-04-08 19:45:43 +0200

    font familly barre de recherche

--- TP3/include/header.inc.php
+++ TP3/include/header.inc.php
@@ -6,14 +6,27 @@
   <head>
     <meta charset="utf-8">
   	<link rel="stylesheet" type="text/css" href="css/stylesheet.css?t=<? echo time(); ?>" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
+font-family: 'Indie Flower', cursive;
 
   </head>
 
   <header>
 
     <img src="image/nb/logo6.png">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <form class="example" action="/action_page.php">
+  <input type="text" placeholder=" Que recherchez-vous?" name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+
     <img src="image/nb/panier.png" class="panier">
 
+
+</div>
     <section class="portfolio-experiment">
       <a>
         <span class="text">Se Connecter</span>
