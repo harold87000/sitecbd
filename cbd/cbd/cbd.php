6352cda2ffdc681bdc4f434d8beba47fbebcaf4f
harold <leroyharold@sfr.fr>
2020-04-08 14:38:21 +0200

    1er commit

---
+++ cbd/cbd/cbd.php
@@ -0,0 +1,37 @@
+<html>
+  <link rel="stylesheet" type="text/css" href="fond.css">
+
+<head>
+
+  </head>
+
+  <header>
+    <img src="logo/logo6.png">
+
+    <img src="logo/panier.png" class="panier">
+    <section class="portfolio-experiment">
+  <a>
+    <span class="text">Se Connecter</span>
+    <span class="line -right"></span>
+    <span class="line -top"></span>
+    <span class="line -left"></span>
+    <span class="line -bottom"></span>
+  </a>
+</section>
+  </header>
+
+<body>
+
+
+
+<?php
+
+//$nom="harold";echo $nom;
+//<input type="text"name="text" value="eriub" />
+
+
+ ?>
+
+
+</body>
+</html>
