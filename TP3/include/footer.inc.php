ec2cc96768469650b8fa033db7a6d51bb67fa758
fares <fares.cherif@etu.unilim.fr>
2020-04-09 18:25:22 +0200

    footer

--- TP3/include/footer.inc.php
+++ TP3/include/footer.inc.php
@@ -1,6 +1,20 @@
+
       </div>
+
+
+
     </body>
   <footer>
-    <p>oui</p>
+      <div class="gauche">
+        <h4>Suivez nous !              </h4>
+        <img src="image/nb/facebook.png" class="facebook">
+      </div>
+      <div class="droite">
+        <h4> Comment nous contacter ?</h4>
+        <img src="image/nb/phone.png" class="facebook">
+
+      </div>
   </footer>
+
+
 </html>
