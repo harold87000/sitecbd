d91bcb27b22f5a2112fcbd76b8c65354e4cab94c
Cherif Fares <fares.cherif@etu.unilim.fr>
2020-04-10 01:00:41 +0200

    logo footer

--- TP3/include/footer.inc.php
+++ TP3/include/footer.inc.php
@@ -18,20 +18,25 @@
         <p>Conditions générales de vente</p>
         <p>Charte graphique</p>
       </div>
+      <div class="cote">
+        <div class="droite">
+          <h4> Comment nous contacter ?</h4>
+          <img src="image/nb/phone.png" class="facebook">
+        </div>
 
-      <div class="droite">
-        <h4> Comment nous contacter ?</h4>
-        <img src="image/nb/phone.png" class="facebook">
-      </div>
-
-      <div class="moyenPaiement">
-        <p>Mention legal</p>
-        <p>CGU</p>
-        <p>Conditions générales de vente</p>
-        <p>Charte graphique</p>
-      </div>
 
   </footer>
 
 
+          <div class="moyenPaiement">
+
+            <img src="image/nb/logovisa.png" class="visa">
+
+
+            <img src="image/nb/logopay.png" class="paypal">
+
+          </div>
+          <img src="image/nb/logovisaelec.png" class="visaelec">
+
+        </div>
 </html>
